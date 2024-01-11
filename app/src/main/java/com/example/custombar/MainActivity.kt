d9f0dbbc86e85bff4a90135a23b0e8eec893b562
AlperenImam0glu <alperenimamoglu16@gmail.com>
2024-01-11 17:19:29 +0300

    animation added

--- app/src/main/java/com/example/custombar/MainActivity.kt
+++ app/src/main/java/com/example/custombar/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.custombar
 
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.view.animation.LayoutAnimationController
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.custombar.databinding.ActivityMainBinding
 
@@ -12,21 +11,30 @@ import com.example.custombar.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
    lateinit var binding: ActivityMainBinding
-    lateinit  var adapter : MyAdapter
+   lateinit  var adapter_power : MyAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-
-        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        binding.rv.layoutManager = layoutManager
-        adapter = MyAdapter(20,5)
-        binding.rv.adapter = adapter
-
-
         setContentView(binding.root)
 
+        binding.rvPower.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        adapter_power = MyAdapter(20,10)
+        binding.rvPower.adapter = adapter_power
+
+        binding.btnStartAnimation.setOnClickListener {
+            startAnimation()
+        }
 
     }
+
+    fun startAnimation(){
+        val resId: Int = R.anim.layout_animation_left_to_right
+        val animation: LayoutAnimationController = AnimationUtils.loadLayoutAnimation(this, resId)
+        binding.rvPower.layoutAnimation = animation;
+        adapter_power.notifyDataSetChanged()
+    }
+
 
 }
--- app/src/main/java/com/example/custombar/MyAdapter.kt
+++ app/src/main/java/com/example/custombar/MyAdapter.kt
@@ -1,19 +1,20 @@
 package com.example.custombar
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.custombar.databinding.RcItemBinding
+import com.example.custombar.databinding.RecyclerviewItemBinding
 
 class MyAdapter(val maxValue: Int, val value: Int) : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
-    val ITEM_COUNT = 44
+     private val ITEM_COUNT = 44
 
-    class ViewHolder(val binding: RcItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    class ViewHolder(val binding: RecyclerviewItemBinding) : RecyclerView.ViewHolder(binding.root) {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = RcItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding =
+            RecyclerviewItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
@@ -23,24 +24,41 @@ class MyAdapter(val maxValue: Int, val value: Int) : RecyclerView.Adapter<MyAdap
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-        val y: Double = ITEM_COUNT.toDouble() / maxValue.toDouble()
-        val border = (value * y).toInt()-1
+        val binding = holder.binding
 
-        Log.e("hata","${y} - ${border} - ${position}")
+        val coefficient: Double = ITEM_COUNT.toDouble() / maxValue.toDouble()
+        val border = (value * coefficient).toInt() - 1
 
-        if (value == 0) {
-            holder.binding.image.setImageResource(R.drawable.bar_8dp)
-            holder.binding.image.alpha = 0.25f
-        } else if (position < border) {
-            holder.binding.image.setImageResource(R.drawable.bar_8dp)
-        } else if (position == border) {
-            holder.binding.image.setImageResource(R.drawable.bar_12dp)
-        } else {
-            holder.binding.image.setImageResource(R.drawable.bar_8dp)
-            holder.binding.image.alpha = 0.25f
+        when {
+            value == 0 -> {
+                updateView(binding.image, R.drawable.bar_8dp, 0.25f)
+            }
+
+            value > maxValue -> {
+                if (position == itemCount - 1) {
+                    updateView(binding.image, R.drawable.bar_12dp, 1.0f)
+                } else {
+                    updateView(binding.image, R.drawable.bar_8dp, 1.0f)
+                }
+            }
+
+            position < border -> {
+                updateView(binding.image, R.drawable.bar_8dp, 1.0f)
+            }
+
+            position == border -> {
+                updateView(binding.image, R.drawable.bar_12dp, 1.0f)
+            }
+
+            else -> {
+                updateView(binding.image, R.drawable.bar_8dp, 0.25f)
+            }
         }
-
     }
 
+    private fun updateView(imageView: ImageView, imageResource: Int, alpha: Float) {
+        imageView.setImageResource(imageResource)
+        imageView.alpha = alpha
+    }
 
 }
